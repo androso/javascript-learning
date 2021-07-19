4ccca0dff17bf39b365482017ab0ce33c461a62a
Anibal <androso421@gmail.com>
2021-07-19 08:03:51 -0600

    finishing dice functionality

--- dom-projects/dice-roll/app.js
+++ dom-projects/dice-roll/app.js
@@ -1,1 +1,12 @@
+const buttonEl = document.querySelector(".dice__trigger");
+const imgEl = document.querySelector(".dice__image");
 const randomNumber = Math.ceil(Math.random() * 6);
+
+function number () {
+    return Math.ceil(Math.random() * 6);
+}
+function changeImg () {
+    imgEl.setAttribute("src", "images/dice" + number() + ".png");
+}
+
+buttonEl.addEventListener("click", changeImg);
