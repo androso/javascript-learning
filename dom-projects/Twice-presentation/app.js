c60ac35a0406221ffe812a23091023479c1a2388
Anibal <androso421@gmail.com>
2021-07-19 14:35:32 -0600

    enabling type a number from 1-4 to choose a song

--- dom-projects/Twice-presentation/app.js
+++ dom-projects/Twice-presentation/app.js
@@ -9,17 +9,22 @@ function unblurBackground(event) {
 		bodyEl.classList.remove("unblur__body");
 	}
 }
-
 function playAlbum (event) {
     if (isAlbum(event)) {
         let song = event.target.parentElement.querySelector("audio");
         song.play();
     }
 }
-
+function playFromKeybr (event) {
+    let albumIndex = parseInt(event.code.match(/\d+/)[0]);
+    if (albumIndex >= 0 && albumIndex <= 4) {
+        albums[albumIndex - 1].parentElement.querySelector("audio").play(); 
+    }
+}
 function isAlbum(event) {
 	return albums.includes(event.target) ? true : false;
 }
 
 bodyEl.addEventListener("mouseover", unblurBackground);
 bodyEl.addEventListener("click", playAlbum);
+window.addEventListener("keydown", playFromKeybr)
