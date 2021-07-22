bcb70dd49d4bc0e9fa08b8bd14e234dfd0e737f1
Anibal <androso421@gmail.com>
2021-07-21 21:00:52 -0600

    finishing styling for the clock

---
+++ dom-projects/JSClock/app.js
@@ -0,0 +1,17 @@
+const hourHandEl = document.querySelector(".hour-hand");
+// for (let i = 30; i <= 360; i+=30) {
+//     hourHandEl.style.transform = `rotate(${i}deg);`;
+//     console.log("is it working?", i);
+
+// }
+
+// // const sleep = (time) => {
+// //     return new Promise((resolve) => setTimeout(resolve, time));
+// // }
+// // const looping = async () => {
+// //     for (let i = 30; i <= 360; i+=30) {
+// //         await sleep(1000);
+// //         console.log(i);
+// //         hourHandEl.style.transform = "rotate(" + i + "deg)";
+// //     }
+// // }
