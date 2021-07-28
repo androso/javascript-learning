af5ac0651d37270f5442d993665328876772b20a
Anibal <androso421@gmail.com>
2021-07-28 08:37:24 -0600

    fixing bug that didn't allow to open again the accordion

--- dom-projects/js-accordion/app.js
+++ dom-projects/js-accordion/app.js
@@ -1,4 +1,3 @@
-
 //Get all the accordion items
 //We add listeners to each accordion heading
 //When that accordion is clicked I want to change the max height of the answer with scrollHeight
@@ -10,14 +9,26 @@ function openAccordion () {
         const accordHeading = item.firstElementChild;
         accordHeading.addEventListener("click", function () {
             const accordDetails = this.nextElementSibling;
+            const accordIcon = this.lastElementChild;
             let toClose = accordDetails.style.maxHeight;
-            if (toClose) { 
-                accordDetails.style.maxHeight = "0";
+
+            
+            if (toClose) {
+                accordIcon.innerHTML = "+";
+                accordDetails.style.maxHeight = null;
             } else {
+                accordIcon.innerHTML = "-";
                 accordDetails.style.maxHeight = accordDetails.scrollHeight + "px";
             }
+            // // if (accordDetails.style.maxHeight === "0px" || accordDetails.style.maxHeight == '') {
+            // //     accordDetails.style.maxHeight = accordDetails.scrollHeight + "px";
+            // // } else if (accordDetails.style.maxHeight) { 
+            // //     accordDetails.style.maxHeight = "0";
+            // // }
         })
     }
 }
 
 openAccordion();
+
+
